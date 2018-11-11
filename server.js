098e11b23cd486c34f405cd28b31bd5f9c132163
mariobermudezjr <mbermudez91@gmail.com>
2018-11-10 20:50:35 -0800

    refactor: Change the folder structure and move register to file by itself

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -18,10 +20,6 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send(dataBase.users);
-});
 
 // Signin user given the correct object (manually for now)
 app.post('/signin', (req, res) => {
@@ -50,35 +48,7 @@ app.post('/signin', (req, res) => {
 
 // Register user given all fields
 app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-
-  // Store hash in your password DB
-  const hash = bcrypt.hashSync(password);
-
-  // Transactions are used to rollback any commits to a DB that would otherwise result in an error.
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(error => res.status(400).json('Unable to register user'));
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 app.get('/profile/:id', (req, res) => {
